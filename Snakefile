8dab2f06c5ca8af819ea049d327abb91998d3984
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2023-08-10 14:07:00 +0200

    first almost working version

---
+++ Snakefile
@@ -0,0 +1,207 @@
+import logging
+
+def activate_logging():
+    logging.basicConfig(
+        format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+        level=logging.INFO)
+
+MASAKHANER_LANGUAGES = [
+    "amh", "hau", "ibo", "kin", "kin", "luo", "pcm", "swa", "wol", "yor"]
+
+NLLB_CODES = {
+    "amh": "amh_Ethi",
+    "hau": "hau_Latn",
+    'ibo': "ibo_Latn",
+    'kin': "kin_Latn",
+    'luo': "luo_Latn",
+    'pcm': "pcm_Latn",
+    'swa': "swh_Latn",
+    'wol': "wol_Latn",
+    'yor': "yor_Latn",
+}
+
+TAG_LIST = [
+    "O", "B-PER", "I-PER", "B-ORG", "I-ORG", "B-LOC", "I-LOC", "B-DATE",
+    "I-DATE"]
+
+SPACY_TYPES = ["PERSON", "ORG", "LOC", "DATE", "GPE"]
+
+
+rule all:
+    input:
+        #expand("masakhaner_data/{lang}/{split}.eng",
+        #       lang=MASAKHANER_LANGUAGES, split=["dev", "test", "train"]),
+        #expand("masakhaner_experiments/spacy/{lang}/{split}-retok.eng",
+        #       lang=MASAKHANER_LANGUAGES, split=["dev", "test"]),
+        "masakhaner_experiments/spacy/amh/test.score",
+
+
+rule download_masakhaner:
+    output:
+        text_file="masakhaner_data/{lang}/{split}.txt",
+        tags_file="masakhaner_data/{lang}/{split}.tags"
+    run:
+        import datasets
+        test_data = datasets.load_dataset("masakhaner", wildcards.lang)['test']
+        f_text = open(output.text_file, "w")
+        f_tags = open(output.tags_file, "w")
+        for item in test_data:
+            f_text.write(
+                " ".join(item['tokens']) + "\n")
+            f_tags.write(
+                " ".join(TAG_LIST[idx] for idx in item['ner_tags']) + "\n")
+
+        f_text.close()
+        f_tags.close()
+
+
+rule translate_input_text:
+    input:
+        src_text="{dataset}/{lang}/{split}.txt"
+    output:
+        tgt_text="{dataset}/{lang}/{split}.eng"
+    resources:
+        mem="40G",
+        cpus_per_task=8,
+        partition="gpu-troja,gpu-ms",
+        flags="--gres=gpu:1"
+    run:
+        from transformers import pipeline
+        import torch
+
+        logger = logging.getLogger("translate")
+        logger.setLevel(logging.INFO)
+
+        logger.info("Loading model.")
+        pipe = pipeline(
+            "translation", model="facebook/nllb-200-3.3B", device=0)
+        logger.info("Model loaded. Loading source text.")
+        with open(input.src_text) as f:
+            src_text = f.readlines()
+        logger.info("Source text loaded. Translating.")
+        translation = pipe(
+            src_text,
+            src_lang=NLLB_CODES[wildcards.lang],
+            tgt_lang="eng_Latn")
+        logger.info("Translation done. Writing to file.")
+        with open(output.tgt_text, "w") as f:
+            for line in translation:
+                print(line['translation_text'], file=f)
+        logger.info("Done.")
+
+
+rule ner_with_spacy:
+    input:
+        "masakhaner_data/{lang}/{split}.eng"
+    output:
+        retokenized="masakhaner_experiments/spacy/{lang}/{split}-retok.eng",
+        tags="masakhaner_experiments/spacy/{lang}/{split}.eng-tags",
+    resources:
+        mem="10G",
+        cpus_per_task=4,
+    run:
+        import spacy
+        logger = logging.getLogger("spacy")
+        logger.setLevel(logging.INFO)
+
+        logger.info("Loading model.")
+        try:
+            nlp = spacy.load("en_core_web_lg")
+        except OSError:
+            logger.info("Model not found. Downloading.")
+            spacy.cli.download("en_core_web_lg")
+            nlp = spacy.load("en_core_web_lg")
+
+        logger.info("Model loaded. Processing input.")
+        f_retokenized = open(output.retokenized, "w")
+        f_tags = open(output.tags, "w")
+        for line in open(input[0]):
+            doc = nlp(line.strip())
+            tokens = []
+            tags = []
+            for token in doc:
+                tokens.append(token.text)
+                if token.ent_iob_ == "O" or token.ent_type_ not in SPACY_TYPES:
+                    tags.append("O")
+                else:
+                    ent_type = token.ent_type_
+                    if ent_type == "PERSON":
+                        ent_type = "PER"
+                    elif ent_type == "GPE":
+                        ent_type = "LOC"
+                    tags.append(token.ent_iob_ + "-" + ent_type)
+            print(" ".join(tokens), file=f_retokenized)
+            print(" ".join(tags), file=f_tags)
+        f_retokenized.close()
+        f_tags.close()
+        logger.info("Done.")
+
+
+rule project_from_eng:
+    input:
+        retokenized="{dataset}_experiments/{experiment}/{lang}/{split}-retok.eng",
+        tags="{dataset}_experiments/{experiment}/{lang}/{split}.eng-tags",
+        target_text="{dataset}_data/{lang}/{split}.txt",
+    output:
+        "{dataset}_experiments/{experiment}/{lang}/{split}.orig-tags"
+    resources:
+        mem="40G",
+        cpus_per_task=8,
+        partition="gpu-troja,gpu-ms",
+        flags="--gres=gpu:1 --constraint='gpuram48G|gpuram40G'"
+    run:
+        from constrained_translate_with_tags import project_markup
+        tags = project_markup(
+            open(input.retokenized).readlines(),
+            open(input.tags).readlines(),
+            open(input.target_text).readlines(),
+            language=NLLB_CODES[wildcards.lang],
+            model="ychenNLP/nllb-200-3.3B-easyproject",
+            max_span_len=5)
+
+        with open(output[0], "w") as f:
+            for line in tags:
+                print(" ".join(line), file=f)
+
+
+rule evaluate_ner:
+    input:
+        system_output="{dataset}_experiments/{experiment}/{lang}/{split}.orig-tags",
+        gold_standard="{dataset}_data/{lang}/{split}.tags",
+    output:
+        score="{dataset}_experiments/{experiment}/{lang}/{split}.score",
+        report="{dataset}_experiments/{experiment}/{lang}/{split}.report"
+    run:
+        import evaluate
+        import json
+        metric = evaluate.load("seqeval")
+
+        with open(input.system_output) as f:
+            outputs = [line.strip().split() for line in f]
+        with open(input.gold_standard) as f:
+            golds = [line.strip().split() for line in f]
+
+        score = metric.compute(predictions=outputs, references=golds)
+
+        with open(output.report, "w") as f:
+            print(json.dumps(score, indent=4), file=f)
+        with open(output.score, "w") as f:
+            print(score['overall_f1'], file=f)
+
+        f_output.close()
+        f_gold.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ constrained_translate_with_tags.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python3
+
+"""Project entity markup from target language (English) to source language.
+
+For each entity in the English, it creates an English sentence with the entity
+marked using square brackets, one entitity at a time. On the output side of the
+translator, we create a list of candidate markup positions and score the
+sentences using a machine-translation system. Then we chose the best scoring
+position and say this is the position of the entity in the source language
+sentence.
+
+"""
+
+from typing import List, Tuple, IO
+import argparse
+import logging
+
+import torch
+from transformers import (
+        AutoTokenizer, AutoModelForSeq2SeqLM, BeamSearchScorer,
+        StoppingCriteriaList, MaxLengthCriteria)
+
+
+logging.basicConfig(
+    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+    level=logging.INFO)
+
+
+def inject_markup(input_str: str, max_len: int) -> Tuple[List[Tuple[int, int]], List[str]]:
+    """Inject markup tokens to get possible translation targets.
+
+    Args:
+    input_str: Tokenized target sentence.
+    max_len: Maximum length of the inserted markup.
+
+    Returns:
+    List of positions and list of sentences with markup.
+    """
+    tokens = input_str.split()
+    positions = []
+    sentences = []
+    for start in range(len(tokens)):
+        for end in range(start + 1, start + max_len):
+            if end > len(tokens):
+                break
+            positions.append((start, end))
+            sentences.append(
+                " ".join(tokens[:start] + ["["] +
+                         tokens[start:end] + ["]"] + tokens[end:]))
+    return positions, sentences
+
+
+def read_entities(file: IO[str]) -> List[List[Tuple[str, int, int]]]:
+    """Read entities as BIO tags and format as spans.
+
+    Args:
+    file: File with entities in BIO format.
+
+    Returns:
+    List of lists of entity spans: type, start, end.
+    """
+    all_entities = []
+    for line in file:
+        entities = []
+        current_entity = None
+        current_start = None
+        for i, tag in enumerate(line.strip().split()):
+            if tag == "O":
+                if current_entity is not None:
+                    entities.append((current_entity, current_start, i))
+                current_entity = None
+            if tag.startswith("B-"):
+                if current_entity is not None:
+                    entities.append((current_entity, current_start, i))
+                current_entity = tag[2:]
+                current_start = i
+        all_entities.append(entities)
+    return all_entities
+
+
+def format_entities(
+        sentences: List[str],
+        entities: List[Tuple[str, int, int]]) -> List[str]:
+    """Format entity spans (type, start, end) as BIO tags.
+
+    Args:
+    sentences: List of sentences in target language.
+    entities: List of entity spans: type, start, end.
+
+    Returns:
+    List of BIO tags.
+    """
+
+    for sentence, spans in zip(sentences, entities):
+        tokens = sentence.split()
+        tags = ["O"] * len(tokens)
+        for ent_type, start, end in spans:
+            tags[start] = f"B-{ent_type}"
+            for i in range(start + 1, end):
+                tags[i] = f"I-{ent_type}"
+        yield " ".join(tags)
+
+
+@torch.no_grad()
+def project_markup(
+        eng_file: List[str],
+        ent_file: List[str],
+        tgt_file: List[str],
+        language: str,
+        model: str = "ychenNLP/nllb-200-3.3B-easyproject",
+        max_span_len: int = 4,):
+    """Project entity markup from target language to source language."""
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    logging.info("Using device: '%s'", device)
+    logging.info("Loading tokenizer '%s'", model)
+    tokenizer = AutoTokenizer.from_pretrained(
+        #model,
+        "facebook/nllb-200-3.3B",
+        src_lang="eng_Latn", tgt_lang=language,)
+
+    logging.info("Loading model '%s'", model)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model).to(device).eval()
+    xent = torch.nn.CrossEntropyLoss(reduction="none")
+    logging.info("Translating...")
+
+    all_entities = read_entities(ent_file)
+    all_projected_entities = []
+    for eng_sent, entities, tgt_sent in zip(eng_file, all_entities, tgt_file):
+
+        markup_positions, markup_sentences = inject_markup(tgt_sent, max_span_len)
+        src_tokens = eng_sent.split()
+
+        projected_entities = []
+        for ent_type, start, end in entities:
+            src_sent = " ".join(
+                src_tokens[:start] +
+                ["["] + src_tokens[start:end] + ["]"] +
+                src_tokens[end:])
+            logging.info("Source sentence: %s", src_sent)
+
+            scores = []
+            tokenized = tokenizer(
+                [src_sent] * len(markup_sentences), text_target=markup_sentences,
+                return_tensors="pt").to(device)
+            logits = model(**tokenized).logits
+            bsz, seq_len, vocab_size = logits.shape
+            # By definitions, all target sequences are the same length, so we
+            # do not need to mask.
+            loss_per_token = xent(
+                logits.view(-1, vocab_size),
+                tokenized["labels"].view(-1)).view(bsz, seq_len)
+            scores = loss_per_token.sum(dim=1).cpu().numpy()
+
+            _, (best_start, best_end) = min(zip(scores, markup_positions))
+            projected_entities.append((ent_type, best_start, best_end))
+        all_projected_entities.append(projected_entities)
+
+    logging.info("Finished translating %d sentences.", len(all_projected_entities))
+    logging.info("Formatting entities as BIO tags...")
+    all_projected_tags = format_entities(tgt_file, all_projected_entities)
+
+    logging.info("Done.")
+    return all_projected_tags
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("eng_file", type=argparse.FileType("r"))
+    parser.add_argument("ent_file", type=argparse.FileType("r"))
+    parser.add_argument("tgt_file", type=argparse.FileType("r"))
+    parser.add_argument("language", type=str)
+    parser.add_argument("--model", type=str, default="ychenNLP/nllb-200-3.3B-easyproject")
+    parser.add_argument("--max_span_len", type=int, default=4)
+    args = parser.parse_args()
+
+    projected_tags = project_markup(
+        args.eng_file.readlines(),
+        args.ent_file.readlines(),
+        args.tgt_file.readlines(),
+        args.language,
+        args.model, args.max_span_len)
+
+    for tags in projected_tags:
+        print(tags)
+
+
+if __name__ == "__main__":
+    main()
